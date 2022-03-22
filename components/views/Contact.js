0e96cfc01bb24c50632b176a3a1da78a4e28e748
Michael Kelly <martisemk@gmail.com>
2022-03-21 23:05:18 -0500

    form added

--- components/views/Contact.js
+++ components/views/Contact.js
@@ -7,5 +7,17 @@ export default () => html`
     </header>
     <p>Leave a message and your contact information</p>
     <p>Were open to discussion, suggestions, and business inquiries</p>
+    <form action="https://formspree.io/f/mwkyqjkb" method="POST">
+      <label>
+        Your email:
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Your message:
+        <textarea name="message"></textarea>
+      </label>
+      <!-- your other form fields go here -->
+      <button type="submit">Send</button>
+    </form>
   </section>
 `;
