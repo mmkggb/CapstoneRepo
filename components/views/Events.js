482e475bda145d42795833fa8bd8a2cf99c1462a
Michael Kelly <martisemk@gmail.com>
2022-03-23 20:50:14 -0500

    new css design 3/23/22

--- components/views/Events.js
+++ components/views/Events.js
@@ -4,12 +4,12 @@ export default () => html`
   <section id="events">
     <header><h2>Select Events</h2></header>
     <table style="width:100%">
-      <tr>
+      <tr class="center">
         <th>NFL</th>
         <th>NBA</th>
         <th>MLB</th>
       </tr>
-      <tr>
+      <tr class="center">
         <td>${SportState.nfl.description}</td>
         <td>NBA DATA</td>
         <td>MLB DATA</td>
