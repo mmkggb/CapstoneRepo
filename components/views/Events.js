c89356bdfa1be7c514fcffc5576797a02946f9d1
Michael Kelly <martisemk@gmail.com>
2022-04-06 19:51:30 -0500

    deleted env

--- components/views/Events.js
+++ components/views/Events.js
@@ -6,22 +6,26 @@ export default st => html`
     <table id="test" style="width:100%">
       <thead>
         <tr>
-          <th>NFL</th>
-          <th>NBA</th>
-          <th>MLB</th>
+          <th colspan="2">NFL</th>
         </tr>
       </thead>
       <tbody>
-        ${st.nba.map(game => {
-          return `<tr><td>${game.strEvent}</td><td>${game.strEvent}</td><td>${game.strEvent}</td></tr>`;
-        })}
-        ${st.mlb.map(game => {
-          return `<tr><td>${game.strEvent}</td><td>${game.strEvent}</td><td>${game.strEvent}</td></tr>`;
-        })}
+        <tr>
+          <td>Game</td>
+          <td>Date</td>
+        </tr>
         ${st.nfl.map(game => {
-          return `<tr><td>${game.strEvent}</td><td>${game.strEvent}</td><td>${game.strEvent}</td></tr>`;
+          return `<tr><td>${game.strEvent}</td><br><td>${game.dateEvent}</td></tr>`;
         })}
       </tbody>
     </table>
   </section>
 `;
+/* <th>NBA</th>
+          <th>MLB</th> */
+//  ${st.nba.map(game => {
+// return `<tr><td>${game.strEvent}</td><td>${game.strEvent}</td><td>${game.strEvent}</td></tr>`;
+// })}
+// ${st.mlb.map(game => {
+// return `<tr><td>${game.strEvent}</td><td>${game.strEvent}</td><td>${game.strEvent}</td></tr>`;
+// })}
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import * as state from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
+import dotenv from "dotenv";
 
+dotenv.config();
 const router = new Navigo("/");
 
 function render(st) {
